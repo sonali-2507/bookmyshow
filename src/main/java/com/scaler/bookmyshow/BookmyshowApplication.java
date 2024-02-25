2f7bf9554b96b9c6179edc631849a7560309105c
sonali <stsonali4@gmail.com>
2024-02-25 22:38:06 +0530

    All file added

---
+++ src/main/java/com/scaler/bookmyshow/BookmyshowApplication.java
@@ -0,0 +1,28 @@
+package com.scaler.bookmyshow;
+
+import com.scaler.bookmyshow.controller.UserController;
+import com.scaler.bookmyshow.dtos.SignUpRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BookmyshowApplication implements CommandLineRunner {
+	@Autowired
+	private UserController userController;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyshowApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		SignUpRequestDTO signUpRequestDTO = new SignUpRequestDTO();
+		signUpRequestDTO.setEmail("anshu@scaler.com");
+		signUpRequestDTO.setPassword("anshu@123");
+		userController.signUp(signUpRequestDTO);
+	}
+
+}
---
+++ src/main/java/com/scaler/bookmyshow/models/ShowSeat.java
@@ -0,0 +1,24 @@
+package com.scaler.bookmyshow.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class ShowSeat extends BaseModel {
+    @ManyToOne
+    private Seat seat;
+    @ManyToOne
+    private Show show;
+
+    @Enumerated(EnumType.ORDINAL)
+    private ShowSeatStatus showSeatStatus;
+    private Date blockedAt;
+}
